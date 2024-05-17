74bed6ebcd2a7f8dbf013692270b0a4e66f2a993
BalaSyamala <llakshmi373@gmail.com>
2024-05-17 15:19:36 -0400

    youtube_page

---
+++ src/Header.js
@@ -0,0 +1,55 @@
+function Header() {
+  return (
+    <div>
+      <img
+        src="https://yt3.googleusercontent.com/tSdmaM5Tx0wzgY5r7Ere2KIDuO_drWJnLBbdCBd4ObB2yXs2tJOL26wZmun_PYAgZk50-u2Vpg=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj"
+        alt="raw"
+      />
+      <div
+        style={{
+          display: "flex",
+          marginLeft: "15px",
+          flexwrap: "wrap",
+          marginRight: "15px",
+        }}
+      >
+        <img
+          style={{
+            borderRadius: "50%",
+          }}
+          src="https://yt3.googleusercontent.com/j37qMFnV5sP0KE_Uho59dLKdqXtlRbdMfpTqLG5CwGTzmeWVMEyDeptC4QgicFaQN5xx3YjYSy4=s176-c-k-c0x00ffffff-no-rj"
+          alt="rawtalks"
+        />
+        <div>
+          <h2>Raw Talks With VK</h2>
+          <p>
+            @rawtalkswithvk .449K subscribers •291 videos<br></br>
+            On a MISSION to build Intellectual Telugu Community!!!
+          </p>
+          <span>
+            <a href="rawtalks.in ">rawtalks.in</a>
+            and 5 more links
+          </span>
+          <br></br>
+          <div>
+            <button
+              style={{
+                border: "none",
+                padding: "10px 20px",
+                borderRadius: "15px",
+                cursor: "pointer",
+                backgroundColor: "#212121",
+                color: "white",
+                marginRight: "10px",
+                marginTop: "15px",
+              }}
+            >
+              subscribe
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Header;
---
+++ src/card.js
@@ -0,0 +1,325 @@
+function Card() {
+  return (
+    <div>
+      <hr></hr>
+      <div
+        style={{
+          marginTop: "15px",
+        }}
+      >
+        <button
+          style={{
+            border: "none",
+            padding: "10px 20px",
+            borderRadius: "15px",
+            cursor: "pointer",
+            backgroundColor: "#212121",
+            color: "white",
+            marginRight: "10px",
+          }}
+        >
+          Latest
+        </button>
+        <button
+          style={{
+            padding: "10px 20px",
+            borderRadius: "15px",
+            color: "black",
+            border: "none",
+            cursor: "pointer",
+            marginBorder: "10 px",
+          }}
+        >
+          Popular
+        </button>
+        <button
+          style={{
+            padding: "10px 20px",
+            borderRadius: "15px",
+            color: "black",
+            border: "none",
+            cursor: "pointer",
+            marginBorder: "10 px",
+          }}
+        >
+          Oldest
+        </button>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "30px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex column",
+            width: "300px",
+            gap: "10px",
+            marginTop: "15px",
+          }}
+        >
+          <img
+            style={{
+              borderRadius: "15px",
+            }}
+            src="https://i.ytimg.com/an_webp/IDzfs62WnD8/mqdefault_6s.webp?du=3000&sqp=CK2JmLIG&rs=AOn4CLCpC0KS2o75NN63-iNc_tQ8nrQkPg"
+            alt="Rana Dagubathi"
+          />
+          <a href="https://www.youtube.com/watch?v=IDzfs62WnD8&t=1s">
+            Rana Daggubati on Project K, S.S Rajamouli, Entrepreneurship & Why
+            80% movies flop?| RawTalks EP-50
+          </a>
+          <div>
+            <ul
+              style={{
+                display: "flex",
+                flexwrap: "Wrap",
+                allignItems: "left",
+              }}
+              type="none"
+            >
+              <li>974K views</li>
+              <li>.12 days ago</li>
+            </ul>
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex column",
+            width: "300px",
+            gap: "10px",
+            marginTop: "15px",
+          }}
+        >
+          <img
+            style={{
+              borderRadius: "15px",
+            }}
+            src="https://i.ytimg.com/an_webp/XZqK-I-kc-Y/mqdefault_6s.webp?du=3000&sqp=CKCtmLIG&rs=AOn4CLA1PGPCD4B5FG_f-c1hr78nr3bPug"
+            alt="KTR"
+          />
+          <a href="https://www.youtube.com/watch?v=XZqK-I-kc-Y&t=1173s">
+            Will KCR become CM again?| Liquor Scam, Kaleshwaram Project, Phone
+            Tapping on RawTalks With VK Ep-49
+          </a>
+          <div>
+            <ul
+              style={{
+                display: "flex",
+                flexwrap: "Wrap",
+                allignItems: "left",
+              }}
+              type="none"
+            >
+              <li>1.1M views</li>
+              <li>.2 Weeks ago</li>
+            </ul>
+          </div>
+        </div>
+
+        <div
+          style={{
+            display: "flex column",
+            width: "300px",
+            gap: "10px",
+            marginTop: "15px",
+          }}
+        >
+          <img
+            style={{
+              borderRadius: "15px",
+            }}
+            src="https://i.ytimg.com/an_webp/6MKVRwItAog/mqdefault_6s.webp?du=3000&sqp=CNCfmLIG&rs=AOn4CLCvZ2lY-FQZ2gDmThgiI3UQAqenyQ"
+            alt="priyanka"
+          />
+          <a href="https://www.youtube.com/watch?v=6MKVRwItAog">
+            Middle Class People Should Watch| Health Insurance in India by Mr.
+            Bhanu Gurram Telugu Podcast Ep-48
+          </a>
+          <div>
+            <ul
+              style={{
+                display: "flex",
+                flexwrap: "Wrap",
+                allignItems: "left",
+              }}
+              type="none"
+            >
+              <li>338K views</li>
+              <li>.2 weeks ago</li>
+            </ul>
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex column",
+            width: "300px",
+            gap: "10px",
+            marginTop: "15px",
+          }}
+        >
+          <img
+            style={{
+              borderRadius: "15px",
+            }}
+            src="https://i.ytimg.com/an_webp/F10_ZLC_oTM/mqdefault_6s.webp?du=3000&sqp=CL61mLIG&rs=AOn4CLBVEBmWO0Mkhz8ku7Ogjn7NL8vXmw"
+            alt="USA Job"
+          />
+          <a href="https://www.youtube.com/watch?v=F10_ZLC_oTM&t=12s">
+            USA లో Job వదిలేసి, Hyderabad లో 250 Crores Turnover | Telugu
+            Business Podcast | Raw Talks with VK
+          </a>
+          <div>
+            <ul
+              style={{
+                display: "flex",
+                flexwrap: "Wrap",
+                allignItems: "left",
+              }}
+              type="none"
+            >
+              <li>790k views</li>
+              <li>.1 month ago</li>
+            </ul>
+          </div>
+        </div>
+
+        <div
+          style={{
+            display: "flex column",
+            width: "300px",
+            gap: "10px",
+            marginTop: "15px",
+          }}
+        >
+          <img
+            style={{
+              borderRadius: "15px",
+            }}
+            src="https://i.ytimg.com/an_webp/ra1__kEJMVo/mqdefault_6s.webp?du=3000&sqp=CMKomLIG&rs=AOn4CLBxYJTsM2nI7cLj913_8EigVIcNYQ"
+            alt="Amarnadh"
+          />
+          <a href="https://www.youtube.com/watch?v=ra1__kEJMVo">
+            "CM Jagan’s Plan? Gudivada Amarnath on Raw Talks | Telugu Business
+            Podcast
+          </a>
+          <div>
+            <ul
+              style={{
+                display: "flex",
+                flexwrap: "Wrap",
+                allignItems: "left",
+              }}
+              type="none"
+            >
+              <li>485K views</li>
+              <li>.1 month ago</li>
+            </ul>
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex column",
+            width: "300px",
+            gap: "10px",
+            marginTop: "15px",
+          }}
+        >
+          <img
+            style={{
+              borderRadius: "15px",
+            }}
+            src="https://i.ytimg.com/an_webp/3_sshFBc5fU/mqdefault_6s.webp?du=3000&sqp=CPSfmLIG&rs=AOn4CLAbIpVk6fvmfuMlvlkqB0V3om_9aQ"
+            alt="JD Lakshmi"
+          />
+          <a href="https://www.youtube.com/watch?v=3_sshFBc5fU">
+            Ex-CBI JD Lakshmi Narayana on Raw Talks|Scams|High Profile
+            Cases|Polavaram|Politicial Podcast| Ep-44
+          </a>
+          <div>
+            <ul
+              style={{
+                display: "flex",
+                flexwrap: "Wrap",
+                allignItems: "left",
+              }}
+              type="none"
+            >
+              <li> 684k views</li>
+              <li>.2 months ago</li>
+            </ul>
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex column",
+            width: "300px",
+            gap: "10px",
+            marginTop: "15px",
+          }}
+        >
+          <img
+            style={{
+              borderRadius: "15px",
+            }}
+            src="https://i.ytimg.com/an_webp/WM5exaOhAuA/mqdefault_6s.webp?du=3000&sqp=CIDAmLIG&rs=AOn4CLCl-jHNOe0fCnzs41xOm6i_AYnDiw"
+            alt="Bala Krishna"
+          />
+          <a href="https://www.youtube.com/watch?v=WM5exaOhAuA&t=11s">
+            RawTalks With Sribharat |GITAM University President on Reality of
+            Politics, DrugHub, Nepotism|Ep-43
+          </a>
+          <div>
+            <ul
+              style={{
+                display: "flex",
+                flexwrap: "Wrap",
+                allignItems: "left",
+              }}
+              type="none"
+            >
+              <li>482k views</li>
+              <li>.2 months ago</li>
+            </ul>
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex column",
+            width: "300px",
+            gap: "10px",
+            marginTop: "15px",
+          }}
+        >
+          <img
+            style={{
+              borderRadius: "15px",
+            }}
+            src="https://i.ytimg.com/an_webp/o2uFEdJehEA/mqdefault_6s.webp?du=3000&sqp=CIC2mLIG&rs=AOn4CLDMPD2EQ2pNC-X9RtPztRfWELpEaA"
+            alt="priyamani"
+          />
+          <a href="https://www.youtube.com/watch?v=o2uFEdJehEA&t=370s">
+            South v/s Bollywood Industry|RawTalks Ft.Priyamani |Reality
+            Shows|Paparazzi|Telugu FilmPodcast Ep-42
+          </a>
+          <div>
+            <ul
+              style={{
+                display: "flex",
+                flexwrap: "Wrap",
+                allignItems: "left",
+              }}
+              type="none"
+            >
+              <li>694k views</li>
+              <li>.2 months ago</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Card;
